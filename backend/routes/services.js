3df0fb6d6d8a94a0623d594923e53ccceda950dc
victor-430 <oluwatobivictor430@gmail.com>
2025-10-10 16:47:00 +0100

    feat(routes): add create service validation for services

---
+++ backend/routes/services.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { body, query } from "express-validator";
+import authorizeRoles from "../middleware/authorization.js";
+import authenticateToken from "../middleware/auth.js";
+
+const serviceRouter = express.Router();
+
+const createServiceValidation = [
+  body("title")
+    .trim()
+    .isLength({ min: 10, max: 100 })
+    .withMessage("Title must be between 10 and 100 characters"),
+  body("description")
+    .trim()
+    .isLength({ min: 50, max: 2000 })
+    .withMessage("Descriptioin must be between 50 and 2000 characters"),
+  body("category")
+    .trim()
+    .withMessage("Invalid category")
+    .isIn([
+      "web development",
+      "mobile development",
+      "graphic design",
+      "content writing",
+      "digital marketing",
+      "video editing",
+      "consulting",
+      "legal",
+      "accounting",
+      "tutoring",
+      "other",
+    ]),
+  body("pricing.amount")
+    .trim()
+    .isFloat({ min: 0 })
+    .withMessage("Amount must be a positive nummber"),
+  body("pricing.type")
+    .isIn(["fixed", "hourly", "negotiable"])
+    .withMessage("Pricing must be fixed, hourly or negotiable"),
+  body("location.type")
+    .optional()
+    .isIn(["remote", "onsite", "hybrid"])
+    .withMessage("Location must be remote, onsite or hybrid"),
+  body("duration").trim().notEmpty().withMessage("Duration is required"),
+];
